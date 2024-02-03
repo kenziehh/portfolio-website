9cb397ce964de7c0ad600dc5965796f4eb4d1b29
kenziehh <ghaisani.nurani@gmail.com>
2024-02-03 23:08:40 +0700

    add skill in porto

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,8 +1,12 @@
+import CategoryCard from "./components/CategoryCard";
 import PortfolioCard from "./components/PortfolioCard";
 
 const Portfolio = () => {
   return (
-    <section className="container py-20 flex flex-col gap-12 h-screen" id="portfolio">
+    <section
+      className="container py-20 flex flex-col gap-12 h-screen"
+      id="portfolio"
+    >
       <h1>My Portfolios</h1>
       <div className="flex justify-center">
         <div className="flex flex-col md:flex-row gap-8 md:gap-20">
@@ -10,6 +14,11 @@ const Portfolio = () => {
             name="CareerHub"
             link="https://github.com/kenziehh/CareerHub"
           >
+            <div className="flex gap-3 mb-4">
+              <CategoryCard>JavaScript</CategoryCard>
+              <CategoryCard>React</CategoryCard>
+              <CategoryCard>Tailwind CSS</CategoryCard>
+            </div>
             <p>
               - Membuat Tampilan website menggunakan bahasa pemrograman
               JavaScript dengan framework React, Vite, dan Tailwind CSS.
@@ -20,14 +29,19 @@ const Portfolio = () => {
             name="DelivEat"
             link="https://github.com/romves/food-delivery-app"
           >
+            <div className="flex gap-3 mb-4">
+              <CategoryCard>Java</CategoryCard>
+              <CategoryCard>Maven</CategoryCard>
+              <CategoryCard>T-SQL</CategoryCard>
+            </div>
             <p>- Merancang database, dan membuat DDL serta DML nya.</p>
             <p>
               - Memprogram sistem dengan menggunakan bahasa pemrograman Java
               baik Front End maupun Back End.
             </p>
             <p>
-              - Membuat flow mulai dari akun dibuat hingga pesanan selesai dengan
-              2 sisi user (Customer dan Restaurant)
+              - Membuat flow mulai dari akun dibuat hingga pesanan selesai
+              dengan 2 sisi user (Customer dan Restaurant)
             </p>
           </PortfolioCard>
         </div>
---
+++ src/components/CategoryCard.tsx
@@ -0,0 +1,5 @@
+const CategoryCard = ({ children }: { children: React.ReactNode }) => {
+  return <div className="p-2 bg-secBlue">{children}</div>;
+};
+
+export default CategoryCard;
